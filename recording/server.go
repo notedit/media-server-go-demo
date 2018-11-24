b73996f518216f5ef4f26bae6235b4555f11f44d
notedit <notedit@gmail.com>
2018-11-24 14:26:58 +0800

    add recording demo

---
+++ recording/server.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"github.com/notedit/media-server-go"
+	"github.com/notedit/media-server-go/sdp"
+)
+
+type Message struct {
+	Cmd string `json:"cmd,omitempty"`
+	Sdp string `json:"sdp,omitempty"`
+}
+
+var upGrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+var Capabilities = map[string]*sdp.Capability{
+	"audio": &sdp.Capability{
+		Codecs: []string{"opus"},
+	},
+	"video": &sdp.Capability{
+		Codecs: []string{"vp8"},
+		Rtx:    true,
+		Rtcpfbs: []*sdp.RtcpFeedback{
+			&sdp.RtcpFeedback{
+				ID: "goog-remb",
+			},
+			&sdp.RtcpFeedback{
+				ID: "transport-cc",
+			},
+			&sdp.RtcpFeedback{
+				ID:     "ccm",
+				Params: []string{"fir"},
+			},
+			&sdp.RtcpFeedback{
+				ID:     "nack",
+				Params: []string{"pli"},
+			},
+		},
+		Extensions: []string{
+			"urn:3gpp:video-orientation",
+			"http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01",
+			"http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time",
+			"urn:ietf:params:rtp-hdrext:toffse",
+			"urn:ietf:params:rtp-hdrext:sdes:rtp-stream-id",
+			"urn:ietf:params:rtp-hdrext:sdes:mid",
+		},
+	},
+}
+
+func channel(c *gin.Context) {
+
+	ws, err := upGrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		return
+	}
+	defer ws.Close()
+
+	var transport *mediaserver.Transport
+	endpoint := mediaserver.NewEndpoint("127.0.0.1")
+
+	for {
+		// read json
+		var msg Message
+		err = ws.ReadJSON(&msg)
+		if err != nil {
+			fmt.Println("error: ", err)
+			break
+		}
+
+		if msg.Cmd == "offer" {
+			offer, err := sdp.Parse(msg.Sdp)
+			if err != nil {
+				panic(err)
+			}
+			transport = endpoint.CreateTransportWithRemote(offer, false)
+			transport.SetRemoteProperties(offer.GetMedia("audio"), offer.GetMedia("video"))
+
+			answer := offer.Answer(transport.GetLocalICEInfo(),
+				transport.GetLocalDTLSInfo(),
+				endpoint.GetLocalCandidates(),
+				Capabilities)
+
+			transport.SetLocalProperties(answer.GetMedia("audio"), answer.GetMedia("video"))
+
+			for _, stream := range offer.GetStreams() {
+				incomingStream := transport.CreateIncomingStream(stream)
+
+				outgoingStream := transport.CreateOutgoingStream(stream.Clone())
+
+				outgoingStream.AttachTo(incomingStream)
+
+				answer.AddStream(outgoingStream.GetStreamInfo())
+			}
+
+			ws.WriteJSON(Message{
+				Cmd: "answer",
+				Sdp: answer.String(),
+			})
+		}
+	}
+}
+
+func main() {
+	bindAddress := "localhost:6000"
+	r := gin.Default()
+	r.GET("/channel", channel)
+	r.Run(bindAddress)
+}
