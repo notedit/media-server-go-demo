1f506bbcd8d630db10ec202538f1f07487004fb0
notedit <notedit@gmail.com>
2018-12-14 16:08:26 +0800

    reduce chunk size

--- webrtc-to-rtmp/rtmp-server/server.js
+++ webrtc-to-rtmp/rtmp-server/server.js
@@ -4,7 +4,7 @@ const { NodeMediaServer } = require('node-media-server');
 const config = {
   rtmp: {
     port: 1935,
-    chunk_size: 1024,
+    chunk_size: 128,
     gop_cache: true,
     ping: 60,
     ping_timeout: 30
